23cc29819976e9dc8bcd360705aa30ed853750ef
MangArya11 <komanggede86@gmail.com>
2021-03-13 12:58:43 +0800

    add parallax effect

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ $(document).ready( function(){
     firstPage();
     disableScroll();
     getList();
+    parallaxEffect();
 })
 
 // Desc Status
@@ -20,6 +21,17 @@ function disableScroll() {
     // Disable user to scroll bottom
 
     $page.scrollTop = $window.scrollY;
+}
+
+function parallaxEffect() {
+    
+    $window.scroll( function scroll(){
+        let height = $window.scrollTop();
+
+        if(height >= 1 && height <= 695) {
+            $('.banner-movie').offset({ top: height / 2 })
+        }
+    });
 }
 
 function firstPage(){
@@ -162,9 +174,15 @@ function movieDetail(movie){
             </div>
             <div class="info-movie">
                 <div class="rating">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>
                     <p>${movie.rating}</p>
                 </div>
                 <div class="time">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                    </svg>
                     <p>${movie.time} min</p>
                 </div>
             </div>
